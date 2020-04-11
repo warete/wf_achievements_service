ca475330ad1f97c92bd1ebb98638ddccad755d88
Egor Borisovsky <egor@intervolga.ru>
2020-04-11 20:27:01 +0400

    Добавлен endpoint для парсинга достижений. Проверка секретного параметра

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import os
 import json
 
-import parser
+from wfparser import run as parser_run
 
 app = Flask(__name__, static_folder='assets')
 # enable CORS
@@ -12,6 +12,7 @@ CORS(app, origins=['warete.lh1.in', 'localhost:3000'])
 OUT_DIR = os.environ.get('OUT_DIR', None)
 BASE_URL = os.environ.get('BASE_URL', None)
 PARSE_ENDPOINT = os.environ.get('PARSE_ENDPOINT', None)
+PARSE_ENDPOINT_SECRET = os.environ.get('PARSE_ENDPOINT_SECRET', None)
 
 
 @app.route('/api/achievements', methods=['GET'])
@@ -27,6 +28,24 @@ def achievements():
         }
 
     return jsonify(response)
+
+
+@app.route('/api/parse_achievements', methods=['GET'])
+def parse_achievements():
+    if request.args.get('secret') == PARSE_ENDPOINT_SECRET:
+        data = parser_run(OUT_DIR, BASE_URL, PARSE_ENDPOINT)
+        objects_count = len(data.mark) + len(data.strip) + len(data.badge)
+        response = {
+            'status': 'ok',
+            'message': 'objects count: ' + objects_count
+        }
+        return jsonify(response)
+    else:
+        response = {
+            'status': 'error',
+            'message': 'access denied'
+        }
+        return jsonify(response), 403
 
 
 if __name__ == '__main__':
